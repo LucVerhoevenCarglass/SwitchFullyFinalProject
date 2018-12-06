10756ae5157e03140439647eb1ff784d96c05baf
SieneDeKeyser <de_keyser_siene@hotmail.com>
2018-12-06 11:59:33 +0100

    Initial setup

---
+++ BackEnd/Swintake/Swintake.domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Swintake.domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ BackEnd/Swintake/Swintake.services.tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Swintake.services.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
