f2694a4ff2877b1d77a7f52b8f8630ad0d4cedd9
Caroline <caroline.callens90@gmail.com>
2018-12-06 15:58:44 +0100

    added modules-skeleton in front-end

--- FrontEnd/Swintake/src/app/app.module.ts
+++ FrontEnd/Swintake/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'; 
+import { CoreModule } from './core/core.module';
+import { RoutingModule } from './routing/routing.module';
+import { FeatureModule } from './feature/feature.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+  
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    RoutingModule,
+    FeatureModule,
+    SharedModule
   ],
-  providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
