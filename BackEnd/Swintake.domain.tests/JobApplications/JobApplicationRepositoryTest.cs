a97c9487a4136870af34c62b33d1422ed5db30b5
SieneDeKeyser <de_keyser_siene@hotmail.com>
2018-12-17 10:41:06 +0100

    Jobapplication create unittests & integrationtests

---
+++ BackEnd/Swintake.domain.tests/JobApplications/JobApplicationRepositoryTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Swintake.domain.Data;
+using Swintake.domain.JobApplications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Swintake.domain.tests.JobApplications
+{
+   public class JobApplicationRepositoryTest
+    {
+        private DbContextOptions<SwintakeContext> _options;
+        private JobApplicationRepository _jobApplicationRepository;
+
+        public JobApplicationRepositoryTest()
+        {
+            //given 
+            _options = new DbContextOptionsBuilder<SwintakeContext>()
+                .UseInMemoryDatabase("swintake" + Guid.NewGuid().ToString("n"))
+                .Options;
+            
+        }
+
+        [Fact]
+        public void GivenNewJobapplication_WhenSavingNewJobapplication_ThenJobapplicationIsSaved()
+        {
+            using (var context = new SwintakeContext(_options))
+            {
+                //Given
+                var newJobApplication = new JobApplicationBuilder()
+                    .WithId(Guid.NewGuid())
+                    .WithCandidateId(Guid.NewGuid())
+                    .WithCampaignId(Guid.NewGuid())
+                    .WithStatus(StatusJobApplication.Active)
+                    .Build();
+
+                _jobApplicationRepository = new JobApplicationRepository(context);
+
+                //When
+                _jobApplicationRepository.Save(newJobApplication);
+
+                //Then
+                Assert.Contains(newJobApplication, context.JobApplications);
+            }
+        }
+    }
+}
