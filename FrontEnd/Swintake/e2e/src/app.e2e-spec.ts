e87a3db733c4b197f89a4fcb79b97a2b1aca3c06
LucVerhoevenCarglass <luc.verhoeven@carglass.be>
2018-12-17 09:33:21 +0100

    20181217_02CandidateIntegrationOnIdTestBack

--- FrontEnd/Swintake/e2e/src/app.e2e-spec.ts
+++ FrontEnd/Swintake/e2e/src/app.e2e-spec.ts
@@ -15,20 +15,20 @@ fdescribe('workspace-project App', () => {
     .expectIfUserIsLoggedIn('Niels');
   });
 
-    it('should add new created campaing into list', () => {
+  it('should add new created campaing into list', () => {
      campaignPage.navigateTo();
      campaignPage.navigateCreateCampaign();
 
      campaignPage.AddNewCampaign(campaignPage.campaign)
                  .expectifCampaignHasbeenAddedToList(campaignPage.campaign.name);
-    });
+  });
 
-    it('should add new created candidate into list', () => {
+  it('should add new created candidate into list', () => {
       candidatePage.navigateTo();
       candidatePage.navigateCreateCandidate();
  
       candidatePage.addNewCandidate(candidatePage.candidate)
                   .expectifCandidateHasbeenAddedToList(candidatePage.candidate.firstName);
-     });
+   });
  
 });
